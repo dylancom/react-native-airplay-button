bf050497628bf2f48e7eca13df9d20c4c737181b
Dylan Companjen <dylancompanjen@gmail.com>
2021-05-07 14:36:45 +0200

    fix: broken example

--- example/ios/AirplayButtonExample/AppDelegate.m
+++ example/ios/AirplayButtonExample/AppDelegate.m
@@ -11,7 +11,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -33,12 +33,12 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  #if DEBUG
+  #ifdef FB_SONARKIT_ENABLED
     InitializeFlipper(application);
   #endif
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"AirplayCastExample"
+                                                   moduleName:@"AirplayButtonExample"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
--- example/ios/File.swift
+++ example/ios/File.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  AirplayCastExample
+//  AirplayButtonExample
 //
 
 import Foundation
